679197315220e75f4d58e1db3268bf72f22d3027
raymag <prayk11@outlook.com>
2021-07-25 23:57:53 -0300

    feat: let there be light

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <div id="header">
+        <h1>VList</h1>
+      </div>
+      <input type="text" placeholder="Task" />
+      <section>
+        <article>
+          <span>Clear Room</span>
+          <button>Delete</button>
+        </article>
+        <article>
+          <span>Clear Room</span>
+          <button>Delete</button>
+        </article>
+        <article>
+          <span>Clear Room</span>
+          <button>Delete</button>
+        </article>
+        <article>
+          <span>Clear Room</span>
+          <button>Delete</button>
+        </article>
+        <article>
+          <span>Clear Room</span>
+          <button>Delete</button>
+        </article>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -22,7 +45,48 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #222;
 }
+html, body {
+  padding: 0;
+  margin: 0;
+}
+
+main {
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+  background: #fff;
+  min-width: 60%;
+}
+#header {
+  width: 100%;
+  background: rgb(36, 246, 211);
+  margin-bottom: 30px;
+  color: #fff;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+}
+
+section {
+  /* width: 100%; */
+  padding: 10px 20px;
+}
+input {
+  width: 90%;
+  font-size: 1.5em;
+}
+article {
+  width: 90%;
+  border: solid 1px #ddd;
+}
+
 </style>
